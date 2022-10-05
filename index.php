49f93e9cadacf250a4ffbb1d03222a47d2060edb
David Alert <david.alert.f@gmail.com>
2022-10-05 12:28:11 +0200

    Examples

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
 <?php
-  echo "<h1>Hola mundo"." o si"."<h1>"
+  echo "<h1>Hola mundo"." o si"."<h1>";
+
+  # GET (Pilla la variable index.php?id=2)
+  if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+  } else {
+    $id = 1;
+  }
+
+  
+
+  # Condicionales
+
+  if ($id == 1){
+    echo "La id es 1";
+  } else {
+    echo "La id no es 1";
+  }
+
+  # Funciones
+  function hola($a){
+    echo "<h1>La función és "."$a"."</h1>";
+  }
+
+  hola("puta");
+
+  #Listas
+
+  $list = ["hola1", "hola2"];
+  echo $list[1];
+
+  #Bucles
+  for ($i = 1; $i <= 10; $i++) {
+    echo $i;
+  }
+
+  foreach ($list as $palabras) {
+    echo "<li>$palabras</li>";
+  }
 ?>
